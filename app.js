805082908e4f861efb56403350c3e38e4ff57659
BULOT <romain.bulot@capgemini.com>
2016-09-21 09:19:39 +0200

    test heroku

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    var remoteAddress = req.headers['x-forwarded-for'] || 
+    				  req.connection.remoteAddress;
+    res.json({ "ipAddress": remoteAddress });
+});
+
+app.listen(process.env.PORT || 80);
